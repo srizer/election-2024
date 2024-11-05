8bf27cd426dce06c8c419a1ab2ab2be82b0b3d34
srizer <samrizer95@gmail.com>
2024-11-05 16:45:03 -0500

    updated with barchart and better font stuff

--- app.py
+++ app.py
@@ -43,6 +43,9 @@ with urllib.request.urlopen("https://gist.githubusercontent.com/srizer/c26b7fb3c
     precincts = json.load(url)
 raw_df = pd.read_csv("https://gist.githubusercontent.com/srizer/5280257d7a6ad8be2f3a8e85524d74a0/raw/d9039edf9d5e3797f4094ed20729e1cc691a258c/Precincts_17(1).csv", skiprows=[0, 1])
 
+# UPLOAD THIS TO GIST EVENTUALLY
+sum_raw = pd.read_csv("https://gist.githubusercontent.com/srizer/087d48dc55a97c3e8c6e9181f2166f9e/raw/9824cbdf50ceada745220306292d1f279a1a90d9/summary_17.csv", skiprows=[0, 1])
+
 cleaned = (
             raw_df.pipe(filter_contest, "Contest Name", ["Representative in Congress (Rep)"])
             .pipe(pivot, "nameplace", "Candidate Name", "Votes")
@@ -76,14 +79,93 @@ cleaned["hover"] = create_hover_text(
                         ]                        
                     )
 
-def display_barchart(df, a, b):
-    a_total = df[a].sum()
-    b_total = df[b].sum()
-    fig = go.Figure(
-        go.Bar(
-            x=[a, b],
-            y=[a_total, b_total]
+cleaned_total_data = np.array([
+                            ["Brian Fitzpatrick", f'{cleaned["Brian Fitzpatrick"].sum():,}', ((cleaned["Brian Fitzpatrick"].sum() / cleaned["Total"].sum()) * 100).round(2).astype(float).astype(str) + "%"],
+                            ["Mark Houck", f'{cleaned["Mark Houck"].sum():,}', ((cleaned["Mark Houck"].sum() / cleaned["Total"].sum()) * 100).round(2).astype(float).astype(str) + "%"],
+                            ["Write-in", f'{cleaned["Write-in"].sum():,}', ((cleaned["Write-in"].sum() / cleaned["Total"].sum()) * 100).round(2).astype(float).astype(str) + "%"],
+                        ])
+cleaned_total_table = pd.DataFrame(cleaned_total_data, columns=["Candidate", "Votes", "Pct"])
+
+# def display_barchart(df, a, b):
+#     a_total = df[a].sum()
+#     b_total = df[b].sum()
+#     fig = go.Figure(
+#         go.Bar(
+#             x=[a, b],
+#             y=[a_total, b_total]
+#         )
+#     )
+#     fig.update_layout(
+#         margin={'t':0,'l':0,'b':0,'r':10}
+#     )
+#     return fig
+
+def display_barchart(df, race):
+    filtered = df[df["Contest Name"] == race]
+    fig = px.bar(
+        filtered,
+        x="Candidate Name",
+        y=["Absentee and Mail-In Ballots Votes", "Election Day Votes", "Provisional Votes"],
+        labels={ 'index': 'Candidate', 'value': 'Number of Votes'}
+    )
+    fig.update_layout(
+        margin={'t':0,'l':0,'b':0,'r':10}
+    )
+    fig.update_layout(
+        font=dict(
+            family="Overpass, monospace",
+            size=14,
+            color="black"
         )
+    )
+    fig.update_layout(
+        hoverlabel=dict(
+            font_color="white"  # Hack to compensate for inability to remove "hover=" from hover
+        )
+    )
+    fig.update_layout(
+        legend_title_text="Types of Ballots",
+        legend=dict(
+            font=dict(
+                family="Overpass, monospace",
+                size=14,
+                color="black"
+            )
+        )
+    )
+    return fig
+
+def display_table(df):
+    fig = go.Figure(
+        go.Table(
+            header=dict(
+                values=list(df.columns),
+                fill_color='white',
+                align=['left', 'right', 'right'],
+                line_color="darkslategray",
+                font=dict(
+                    family="Overpass, monospace",
+                    size=18,
+                    color="black",
+                    weight="bold"
+                )
+            ),
+            cells=dict(
+                values=[df.Candidate, df.Votes, df.Pct],
+                fill_color='white',
+                align=['left', 'right', 'right'],   
+                line_color="darkslategray",
+                font=dict(
+                    family="Overpass, monospace",
+                    size=18,
+                    color="black"
+                ),
+                height=30      
+            )
+        )
+    )
+    fig.update_layout(
+        margin={'t':0,'l':0,'b':0,'r':10}
     )
     return fig
 
@@ -99,7 +181,8 @@ def display_choropleth(df, z):
         center={ "lat": 40.309659650050946, "lon": -75.11666494467029 },
         zoom=9.5,
         hover_data={ "Binned Lead": False, "nameplace": False, "hover": True },
-        # hover_name=" "
+        opacity=0.6,
+        category_orders={ "Binned Lead": labels}
     )
     fig.update_geos(fitbounds="locations", visible=False)
     fig.update_layout(hovermode="x")
@@ -125,13 +208,18 @@ def display_choropleth(df, z):
 app = Dash(__name__, external_stylesheets=['assets/style.css'])
 
 app.layout = html.Div([
-    html.H1('Republican Primary for Congress in Bucks County'),
+    html.H1("Unofficial Bucks County Election Dashboard", style={'textAlign': 'center'}),
+    html.H4("Pre-Release, Not Current 2024 General Election Data", style={'textAlign': 'center'}),
     html.Div([
-        dcc.Graph(figure=display_barchart(cleaned, "Brian Fitzpatrick", "Mark Houck"), config={'displayModeBar': False})
-    ], style={'width': '40%', 'display': 'inline-block'}),
-    html.Div([
-        dcc.Graph(figure=display_choropleth(cleaned, "Binned Lead"), style={'height': '90vh'}, config={'displayModeBar': False}),
-    ], style={'width': '60%', 'display': 'inline-block', 'padding': '0 20'})     
+        html.Div([
+            html.H3('Republican Primary for Congress in Bucks County'),
+            dcc.Graph(figure=display_table(cleaned_total_table), config={'displayModeBar': False}, style={ 'height': '25vh' }),
+            dcc.Graph(figure=display_barchart(sum_raw, "Representative in Congress (Rep)"), config={'displayModeBar': False}, style={ 'height': '45vh' })
+        ], style={'width': '40%', 'display': 'inline-block'}),
+        html.Div([
+            dcc.Graph(figure=display_choropleth(cleaned, "Binned Lead"), style={'height': '80vh'}, config={'displayModeBar': False}),
+        ], style={'width': '60%', 'display': 'inline-block', 'padding': '0 20'})    
+    ], style={'display': 'flex'})
 ])
 
 server = app.server
